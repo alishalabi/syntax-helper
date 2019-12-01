578d911cfa2b42d273dd4dba580ef917a24afd45
Ali Shalabi <ashalabi13@gmail.com>
2019-11-30 19:49:50 -0800

    Working on first subcommand: golang

--- cmd/root.go
+++ cmd/root.go
@@ -20,8 +20,8 @@ import (
   "os"
   "github.com/spf13/cobra"
 
-  homedir "github.com/mitchellh/go-homedir"
-  "github.com/spf13/viper"
+  // homedir "github.com/mitchellh/go-homedir"
+  // "github.com/spf13/viper"
 
 )
 
@@ -32,13 +32,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
   Use:   "syntax-helper",
-  Short: "A brief description of your application",
-  Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+  Short: "CLI to provide code syntax",
+  Long: `This project is designed to help coders get access to quick and proper code syntax
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+The first language to be integrated is Golang (https://golang.org/).`,
   // Uncomment the following line if your bare application
   // has an action associated with it:
   //	Run: func(cmd *cobra.Command, args []string) { },
@@ -53,45 +50,44 @@ func Execute() {
   }
 }
 
-func init() {
-  cobra.OnInitialize(initConfig)
-
-  // Here you will define your flags and configuration settings.
-  // Cobra supports persistent flags, which, if defined here,
-  // will be global for your application.
-
-  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.syntax-helper.yaml)")
-
-
-  // Cobra also supports local flags, which will only run
-  // when this action is called directly.
-  rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-  if cfgFile != "" {
-    // Use config file from the flag.
-    viper.SetConfigFile(cfgFile)
-  } else {
-    // Find home directory.
-    home, err := homedir.Dir()
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
-
-    // Search config in home directory with name ".syntax-helper" (without extension).
-    viper.AddConfigPath(home)
-    viper.SetConfigName(".syntax-helper")
-  }
-
-  viper.AutomaticEnv() // read in environment variables that match
-
-  // If a config file is found, read it in.
-  if err := viper.ReadInConfig(); err == nil {
-    fmt.Println("Using config file:", viper.ConfigFileUsed())
-  }
-}
-
+// func init() {
+//   cobra.OnInitialize(initConfig)
+//
+//   // Here you will define your flags and configuration settings.
+//   // Cobra supports persistent flags, which, if defined here,
+//   // will be global for your application.
+//
+//   rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.syntax-helper.yaml)")
+//
+//
+//   // Cobra also supports local flags, which will only run
+//   // when this action is called directly.
+//   rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+// }
+//
+//
+// // initConfig reads in config file and ENV variables if set.
+// func initConfig() {
+//   if cfgFile != "" {
+//     // Use config file from the flag.
+//     viper.SetConfigFile(cfgFile)
+//   } else {
+//     // Find home directory.
+//     home, err := homedir.Dir()
+//     if err != nil {
+//       fmt.Println(err)
+//       os.Exit(1)
+//     }
+//
+//     // Search config in home directory with name ".syntax-helper" (without extension).
+//     viper.AddConfigPath(home)
+//     viper.SetConfigName(".syntax-helper")
+//   }
+//
+//   viper.AutomaticEnv() // read in environment variables that match
+//
+//   // If a config file is found, read it in.
+//   if err := viper.ReadInConfig(); err == nil {
+//     fmt.Println("Using config file:", viper.ConfigFileUsed())
+//   }
+// }
